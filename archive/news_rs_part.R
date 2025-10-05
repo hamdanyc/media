2dc7097ddd82b40e2c3be5898c6d4f44607159c0
hamdan <hamdan.hy@gmail.com>
2025-10-05 08:06:19 +0800

    house-keeping

---
+++ archive/news_rs_part.R
@@ -0,0 +1,25 @@
+# news_rs_part.R
+
+# server rselenium ----
+remDr <- RSelenium::remoteDriver(remoteServerAddr = "172.17.0.2",
+                                 port = 4444L,
+                                 browserName = "firefox")
+
+remDr$open()
+
+rsnews <- c("agendadaily",
+             "beritaharian",
+             "hmetro",
+             "nst",
+             "thestar")
+
+n <- 0
+for (i in rsnews) {
+  cat(i,fill = TRUE,sep = " ")
+  try(source(paste0(i,".R")),
+      silent=TRUE)
+}
+
+# close rselenium server----
+remDr$close()
+# remDr$closeServer()
