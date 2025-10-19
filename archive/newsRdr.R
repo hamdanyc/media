165d39d27d700181f81abb69aec1362cb86cfbe9
hamdan <hamdan.hy@gmail.com>
2025-10-19 15:04:35 +0800

    Edited

--- archive/newsRdr.R
+++ archive/newsRdr.R
@@ -2,6 +2,8 @@
 
 library(rmarkdown)
 
+# Sys.setenv(RSTUDIO_PANDOC="/usr/share/positron/resources/app/quarto/bin/tools/x86_64")
+
 # render the default (first) format defined in the file
 render("newsClipbySection.Rmd", output_format = "html_document", 
        output_file = "sorotan.html")
